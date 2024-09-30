e059fdb4913939e8f8339e357901e1ea7b351a6a
TaiwoEnoch <taiwoenoch0@gmail.com>
2024-09-30 13:46:22 +0100

    File structuring

---
+++ src/data/Teams.js
@@ -0,0 +1,22 @@
+const Team = [
+  {
+    id: 1,
+    name: 'Team 1',
+    members: ['John', 'Doe']
+  },
+
+  {
+    id: 2,
+    url: 'https://www.google.com',
+    name: 'Team 2',
+    members: ['Jane', 'Doe']
+  },
+
+  {
+    id: 3,
+    name: 'Team 3',
+    members: ['John', 'Smith']
+  }
+]
+
+export default Team;
